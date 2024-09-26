fabab347965a897f416e2121b19122aeb40e48e5
haihmvnext <129985544+haihmvnext@users.noreply.github.com>
2024-09-26 17:23:48 +0700

    update

--- src/features/choose-card/index.jsx
+++ src/features/choose-card/index.jsx
@@ -3,9 +3,17 @@ import classNames from 'classnames/bind';
 import logo from 'assets/images/text-logo.png';
 import bgHeader from 'assets/images/bg-header.png';
 import divider from 'assets/images/divider.png';
+import card1 from 'assets/images/card1.png';
+import card2 from 'assets/images/card2.png';
+import card3 from 'assets/images/card3.png';
+import square from 'assets/images/square.png';
+import mImg from 'assets/images/m.png';
+import omega from 'assets/images/omega.png';
+import khampha from 'assets/images/khampha.png';
 import { Image, Space } from 'antd';
 
 const cx = classNames.bind(style);
+const listImage = [card1, card2, card3];
 export default function ChooseCard() {
     return (
         <div className= {cx('container')}>
@@ -21,6 +29,20 @@ export default function ChooseCard() {
                 <Image preview={false} src={bgHeader} alt='logo' width={372} />
             </div>
             <Image preview={false} src={divider} alt='logo' height={13} />
+            <div className={cx('content')}>
+                <Image preview={false} src={square} alt='square' width={120} />
+                <Image preview={false} src={khampha} alt='khampha' width={120} />
+                <Image preview={false} src={mImg} alt='m' width={120} />
+                <Image preview={false} src={omega} alt='omega' width={120} />
+                <div className={cx('image-wrapper')}>
+                    {
+                        listImage.map((item, index) => (
+                            <Image key={index} preview={false} src={item} alt='card' width={120} />
+                        ))
+                    }
+                </div>
+            </div>
+            <div className={cx('footer')} />
         </div>
     )
 }
